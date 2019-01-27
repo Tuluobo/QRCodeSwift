c6a2635c376056d85e3038f234abe36e45d8d77f
Tuluobo <tuluobo@vip.qq.com>
2019-01-27 11:43:00 +0800

    Initial Commit

---
+++ QRCodeSwift/QRCodeSwift.h
@@ -0,0 +1,19 @@
+//
+//  QRCodeSwift.h
+//  QRCodeSwift
+//
+//  Created by Hao Wang on 2019/1/27.
+//  Copyright © 2019 Tuluobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRCodeSwift.
+FOUNDATION_EXPORT double QRCodeSwiftVersionNumber;
+
+//! Project version string for QRCodeSwift.
+FOUNDATION_EXPORT const unsigned char QRCodeSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRCodeSwift/PublicHeader.h>
+
+
